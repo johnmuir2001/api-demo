83f7dc0b40f8b87e8a8b1cd6c59281e781d8127f
John Muir <johnmuir@JohnsWorkLaptop.lan>
2022-05-18 11:50:38 +0100

    animal example

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import { useState, useEffect } from "react";
 import './App.css';
-// import AnimalModal from "./AnimalModal";
+import AnimalModal from "./AnimalModal";
 
 const App = () => {
-    // const [allAnimals, setAllAnimals] = useState([]);
-    // const [currentAnimal, setCurrentAnimal] = useState({});
-    // const [showModal, setShowModal] = useState(false);
+    const [allAnimals, setAllAnimals] = useState([]);
+    const [currentAnimal, setCurrentAnimal] = useState({});
+    const [showModal, setShowModal] = useState(false);
     const [showError, setShowError] = useState("");
 
     useEffect(() => {
       const fetchAnimals = async () => {
         try {
-          const response = await fetch("https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles", {
-            method: "POST",
-            headers: {
-              "x-api-key": "PYfVzyOY6i1xzixnh60rI6aVO0XMrn4Gaa2uYecx",
-              "Content-Type": "application/json"
-            },
-            body: {
-              "registrationNumber": "TE57VRN"
-            }
-          });
+          const response = await fetch("https://zoo-animal-api.herokuapp.com/animals/rand/10");
           if(!response.ok){
             throw new Error(response.statusText)
           }
           const data = await response.json()
-          console.log(data)
-          // setAllAnimals(data)
+          setAllAnimals(data)
         } catch (err) {
           setShowError(err.message)
         }
@@ -35,21 +25,21 @@ const App = () => {
       fetchAnimals()
     }, []);
 
-    // const handleClick = (animalObj) => {
-    //   setCurrentAnimal(animalObj);
-    //   setShowModal(true)
-    // }
+    const handleClick = (animalObj) => {
+      setCurrentAnimal(animalObj);
+      setShowModal(true)
+    }
 
   return (
     <div className="App">
       <h1>Animal Encyclopedia</h1>
       <h2>{showError}</h2>
-      {/* <div className="animalWrap">
+      <div className="animalWrap">
         {allAnimals.map((animal, index) => {
           return <img key={index} src={animal.image_link} alt={`${animal.name}`} onClick={() => handleClick(animal)}/>
         })}
       </div>
-      {showModal && <AnimalModal closeModal={setShowModal} animal={currentAnimal}/>} */}
+      {showModal && <AnimalModal closeModal={setShowModal} animal={currentAnimal}/>}
     </div>
   );
 }
